a18b751e2e5e8a12cf6ad49faef112f1d9459d6f
d-rivers <dan.rivers.js@gmail.com>
2019-01-11 04:35:38 +0000

    position/instruction toggle

--- index.js
+++ index.js
@@ -1,16 +1,54 @@
 const vorpal = require('vorpal')();
+const Navigation = require('./lib/navigation');
 
-const action = (command, callback) => {
-  vorpal.activeCommand.log(command);
-  callback();
+const StratumFive = class StratumFive {
+  constructor() {
+    const t = this;
+
+    t.navigation = new Navigation({});
+  }
+
+  init() {
+    const t = this;
+
+    vorpal
+      .mode('navigation')
+      .description('Enter the user into navigation mode.')
+      .delimiter('navigation:')
+      .action(t.action.bind(t));
+
+    vorpal
+      .delimiter('StratumFive$')
+      .show();
+  }
+
+  async action(command, callback) {
+    const t = this;
+    const { activeCommand } = vorpal;
+
+    const validation = await t.navigation
+      .validate(command)
+      .then(response => response)
+      .catch(error => error);
+
+    if (!validation.valid) {
+      callback();
+    }
+
+    const next = t.navigation.line === 0 ? 'positioning' : 'instructions';
+
+    const exe = await t.navigation[next](command)
+      .then(response => response)
+      .catch(error => error);
+
+    activeCommand.log('exe', exe);
+
+    t.navigation.toggle();
+
+    callback();
+  }
 };
 
-vorpal
-  .mode('navigation')
-  .description('Enter the user into navigation mode.')
-  .delimiter('navigation:')
-  .action(action);
+const stratumFive = new StratumFive();
 
-vorpal
-  .delimiter('StratumFive$')
-  .show();
+stratumFive.init();
