255578a0f22792cdf87491bd1a0469b8474d1d33
d-rivers <dan.rivers.js@gmail.com>
2019-01-22 01:02:03 +0000

    rm manual input

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
+const vorpal = require('vorpal');
+const es = require('event-stream');
+
 const Challenge = require('./lib/challenge');
 
-const sf = new Challenge({});
+const sf = new Challenge({
+  vorpal,
+  es,
+});
 
 sf.init();
